24740f3bdfff9bd26b42d6809bcaf1228d1d11de
Frank Solleveld <fsolleveld@outlook.com>
2021-05-24 09:16:49 +0200

    base UI

--- Project2/ContentView.swift
+++ Project2/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Guess the Flag")
+        ZStack {
+            Color.green.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        // flag was tapped
+                    }, label: {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    })
+                }
+                Spacer()
+            }
+        }
     }
 }
 
