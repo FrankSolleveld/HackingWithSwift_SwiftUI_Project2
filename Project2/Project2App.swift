85bbbda9e58f6affe30e85fddeaa9d3c7a9298bc
Frank Solleveld <fsolleveld@outlook.com>
2021-05-24 08:49:46 +0200

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Frank Solleveld on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
